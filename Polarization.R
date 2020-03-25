70e356782cdbbdbfc03defd7917e999f96aec584
Gus Beringer <gusberinger@gmail.com>
2020-03-25 14:53:46 -0500

    finished conversion

--- Polarization.R
+++ Polarization.R
@@ -1,22 +1,77 @@
-## import manifesto project data, ignore missing values, standardize rile pervote and convert election date
-# manifesto_df = pd.read_csv("MPDataset_MPDS2019b.csv")
-# needed_columns = ['edate', 'countryname', 'country', 'pervote', 'rile', 'party', 'partyabbrev']
-# manifesto_df = manifesto_df.dropna(subset=needed_columns)
-# manifesto_df["zrile"] = stats.zscore(df["rile"])
-# manifesto_df["pervote"] = manifesto_df["pervote"] / 100
-# manifesto_df["year"] = pd.to_datetime(manifesto_df["edate"]).dt.year
 setwd("~/Documents/manifestos/")
+rm(list=ls())
+gc()
+
 library(tidyr)
+library(dplyr)
 library(ggplot2)
+library(haven)
+library(MASS)
+library(texreg)
+
 
 manifesto.df <- read.csv("MPDataset_MPDS2019b.csv")
 manifesto.df <- manifesto.df %>% drop_na(c("rile", "pervote"))
 manifesto.df$rile <- scale(manifesto.df$rile)
 manifesto.df$pervote <- manifesto.df$pervote / 100
-manifesto.df$date <- as.Date(manifesto.df$edate, "%d/%m/%y")
+manifesto.df$date <- as.Date(manifesto.df$edate, "%d/%m/%Y")
 manifesto.df$year <- as.numeric(format(manifesto.df$date, "%Y"))
+manifesto.df <- manifesto.df[, c(1, 2, 3, 4, 5, 6, 175, 7:174)]
 
-qplot()
 
-#needed.columns <- c('edate', 'countryname', 'pervote', 'rile', 'party', 'partyabbrev')
-#s <- na.omit(manifesto.df, cols="countryname")
+poldf <- data.frame(year=as.numeric(),
+                    country=as.integer(), 
+                    countryname=character(), 
+                    manifesto.polarization=double(),
+                    stringsAsFactors=FALSE)
+
+
+for (x in unique(manifesto.df$date)) {
+  election.df <- subset(manifesto.df, date == x, select = c("country", "countryname", "partyabbrev", "pervote", "rile", "year"))
+  for (countryid in unique(election.df$country)) {
+    country.df <- subset(election.df, country == countryid)
+    winner.index <- which.max(country.df$pervote)
+    winner <- country.df[winner.index,]
+    losers <- country.df[-c(winner.index), ]
+    losers$t <- losers$pervote * losers$rile
+    polarization <- (winner$rile - sum((losers$pervote * losers$rile) / (1 - winner$pervote))) ** 2
+    poldf[nrow(poldf) + 1,] <- list(winner$year, countryid, as.character(winner$countryname), polarization)
+  }
+}
+poldf <- poldf[order(poldf$country, poldf$year),]
+qplot(year, polarization, data = subset(poldf, countryname == "France"), geom="line")
+head(poldf)
+
+dpi.df <- read_dta("DPI2017.dta")
+dpi.df <- dpi.df %>% rename(dpi.polarization = "polariz")
+dpi.df <- dpi.df[, c("dpi.polarization", "countryname", "year")]
+dpi.df <- dpi.df %>% drop_na("dpi.polarization")
+dpi.df$dpi.polarization <- ordered(as.factor(dpi.df$dpi.polarization))
+
+
+# no integer country code in DPI
+dpi.df$countryname[dpi.df$countryname == "Czech Rep."] <- "Czech Republic"
+dpi.df$countryname[dpi.df$countryname == "GDR"] <- "German Democratic Republic"
+dpi.df$countryname[dpi.df$countryname == "FRG/Germany"] <- "Germany"
+dpi.df$countryname[dpi.df$countryname == "USA"] <- "United States"
+dpi.df$countryname[dpi.df$countryname == "UK"] <- "United Kingdom"
+dpi.df$countryname[dpi.df$countryname == "Macedonia"] <- "North Macedonia"
+dpi.df$countryname[dpi.df$countryname == "Bosnia-Herz"] <- "Bosnia-Herzegovina"
+dpi.df$countryname[dpi.df$countryname == "S. Africa"] <- "South Africa"
+dpi.df$countryname[dpi.df$countryname == "S. Korea"] <- "South Korea"
+
+
+merged <- merge(poldf, dpi.df)
+probit.model <- polr(dpi.polarization~manifesto.polarization, data=merged, method="probit")
+p <- 2*pt(-abs(1.761),df=nrow(merged) - 1)
+summary(probit.model)
+
+
+texreg(probit.model,
+       stars = c(0.01,  0.05, 0.1),
+       booktabs=TRUE,
+       float.pos = "h!",
+       dcolumn = TRUE,
+       digits=4,
+       custom.model.names = "Ordered Probit",
+       custom.coef.names = "Manifesto Polarization")
