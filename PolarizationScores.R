4b773cc76f7dd46c6079a578201e36f35af5810c
Gus Beringer <gusberinger@gmail.com>
2020-04-20 22:03:37 -0500

    Added rules for selecting winner

--- PolarizationScores.R
+++ PolarizationScores.R
@@ -22,6 +22,14 @@ manifesto <- manifesto %>% drop_na(pervote)
 manifesto <- manifesto %>% filter(!is.na(rile) | pervote > .10)
 
 
+## Weird things in manifesto data ------
+
+range <- function(x) {return (max(x) - min(x))}
+
+manifesto %>% group_by(countryname, partyname) %>% 
+  summarize(diff = range(rile)) %>% arrange(desc(diff))
+
+
 ##  Find the Polarization for each election ------------------------------
 
 get_parl_winner <- function(countryname, date) {
@@ -36,7 +44,109 @@ get_parl_winner <- function(countryname, date) {
          },
          "Belgium" = {
            if (year %in% c(1946, 1954)) {return ("Belgian Socialist Party")}
-         }
+           if (year %in% c(1968, 1974, 1977)) {return ("Christian People’s Party")}
+           if (year %in% c(2010)) {return ("Francophone Socialist Party")}
+         },
+         "Bulgaria" = {
+           if (year %in% c(1994)) {return ("Union of Democratic Forces")}
+         },
+         "Canada" = {
+           if (year %in% c(1957, 1979)) {return ("Progressive Conservative Party")}
+         },
+         "Cyprus" = {
+           if (year %in% c(1999)) {return ("Democratic Coalition")}
+           if (year %in% c(2001)) {return ("Democratic Party")}
+         },
+         "Denmark" = {
+           if (date == "1953-04-21" | year %in% c(1945, 1950, 1973, 2015)) {return ("Liberals")}
+           if (year %in% c(1968)) {return ("Danish Social-Liberal Party")}
+           if (year %in% c(1981, 1984, 1987, 1988)) {return ("Conservative People’s Party")}
+           if (year %in% c(2011)) {return ("Social Democratic Party")}
+         },
+         "Estonia" = {
+           if (year %in% c(1999, 2003)) {return ("Estonian Reform Party")}
+           if (year %in% c(2015)) {return ("Estonian Center Party")}
+         },
+         "Finland" = {
+           if (year %in% c(1945)) {return ("Finnish People’s Democratic Union")}
+           # if (year %in% c(1948)) {return ("Finnish Centre")}
+           if (year %in% c(1951)) {return ("Agrarian Union")}
+         },
+         "France" = {
+           if (year %in% c(1946, 1951)) {return ("French Section of the Workers' International")}
+         },
+         "Germany" = {
+           if (year %in% c(1969, 1976)) {return ("Social Democratic Party of Germany")}
+         },
+         "Iceland" = {
+           if (year %in% c(1946, 1956, 1959, 1978)) {return ("Social Democratic Party")}
+           if (year %in% c(1971, 1983, 1987, 2013)) {return ("Progressive Party")}
+           if (year %in% c(2007)) {return ("The Alliance")}
+           if (year %in% c(2017)) {return ("Left Green Movement")}
+         },
+         "Israel" = {
+           if (year %in% c(1984, 1996, 1999, 2009)) {return ("The Consolidation")}
+         },
+         "Latvia" = {
+           if (year %in% c(1998)) {return ("Latvian Way Union")}
+           if (year %in% c(2002)) {return ("People’s Party")}
+           if (year %in% c(2006)) {return ("New Era")}
+           if (year %in% c(2011, 2018)) {return ("Unity")}
+           if (year %in% c(2014)) {return ("Greens' and Farmers’ Union")}
+         },
+         "Lithuania" = {
+           # if (year %in% c(2000, 2012)) {return ("A. Brazauskas Social Democratic Coalition")}
+           if (year %in% c(2016)) {return ("Lithuanian Peasant and Green Union")}
+         },
+         "Luxembourg" = {
+           if (year %in% c(1948, 1964)) {return ("Christian Social People’s Party")}
+           if (year %in% c(1974, 2013)) {return ("Democratic Party")}
+         },
+         "Netherlands" = {
+           if (year %in% c(1948)) {return ("Labour Party")}
+           if (year %in% c(1956)) {return ("Catholic People’s Party")}
+           if (year %in% c(1963, 1971)) {return ("Anti-Revolutionary Party")}
+           if (year %in% c(1977, 1982)) {return ("Christian Democratic Appeal")}
+         },
+         "New Zealand" = {
+           if (year %in% c(1971, 1978)) {return ("New Zealand National Party")}
+           if (year %in% c(2017)) {return ("New Zealand Labour Party")}
+         },
+         "Norway" = {
+           if (year %in% c(1965)) {return ("Centre Party")}
+           if (year %in% c(1969, 2001)) {return ("Christian People’s Party")}
+           if (year %in% c(1981, 2013, 2017)) {return ("Conservative Party")}
+         },
+         "Portugal" = {
+           if (year %in% c(1979)) {return ("Social Democratic Party")}
+           if (year %in% c(2015)) {return ("Socialist Party")}
+         },
+         "Romania" = {
+           if (year %in% c(2004)) {return ("Justice and Truth Alliance")}
+           if (year %in% c(2016)) {return ("Alliance of Liberals and Democrats")}
+         },
+         "Slovakia" = {
+           if (year %in% c(1990)) {return ("Christian Democratic Movement")}
+           # TODO
+         },
+        "Slovenia" = {
+          if (year %in% c(1992)) {return ("Slovenian People's Party")}
+        },
+        "Spain" = {
+          if (year %in% c(2016)) {return ("Spanish Socialist Workers’ Party")}
+        },
+        "Sweden" = {
+          if (year %in% c(1976)) {return ("People’s Party")}
+          if (year %in% c(1979)) {return ("Centre Party")}
+          if (year %in% c(1991, 2006, 2010)) {return ("Moderate Coalition Party")}
+        },
+        "Turkey" = {
+          if (year %in% c(1995)) {return ("Democratic Left Party")}
+        },
+        "United Kingdom" = {
+          if (year %in% c(1951)) {return ("Conservative Party")}
+          if (year %in% c(1974)) {return ("Labour Party")}
+        }
   )
   return(NA)
 }
@@ -52,6 +162,10 @@ get_polarization <- function(df, winner_name) {
   #' define the polarization for a distinct election
   winner <- df %>% filter(partyname == winner_name)
   losers <- df %>% filter(partyname != winner_name)
+  if (nrow(winner) != 1) {
+    print("ERROR")
+    print(winner_name)
+  }
   polarization <- (winner$rile - sum((losers$pervote * losers$rile) / (1 - winner$pervote), na.rm=TRUE)) ** 2
   if (sum(df$pervote, na.rm=TRUE) < .60) {
     return(NA)
@@ -59,19 +173,17 @@ get_polarization <- function(df, winner_name) {
   return(polarization)
 }
 
-
 # map polarization to each election group
 manifesto.scores <- manifesto %>% 
   group_by(countryname, date) %>% 
   nest() %>%
   mutate(naive_winner = map(data, get_naive_winner),
-         # naive_winner = unlist(naive_winner),
          parl_winner = map2(countryname, date, get_parl_winner),
          winner = coalesce(parl_winner, naive_winner),
          polarization = map2_dbl(data, winner, get_polarization),
+         naive_polarization = map2_dbl(data, naive_winner, get_polarization),
          year = as.numeric(format(date, "%Y"))) %>%
   arrange(countryname, date)
-
 manifesto.scores <- manifesto.scores %>% mutate(year = as.numeric(format(date, "%Y")))
 
 
@@ -87,25 +199,29 @@ parl <- parl %>%
 parl.winner <- parl %>% 
   group_by(countryname, date) %>%
   top_n(1, start_date) %>% select(countryname, date, party_name_english)
-manifesto.scores$naive_winner <- unlist(manifesto.scores$naive_winner)
-manifesto.winner <- manifesto.scores %>% select(countryname, date, naive_winner)
-compare.leader <- merge(parl.winner, manifesto.winner) %>%
-  select(countryname, date, naive_winner, party_name_english)
+# manifesto.scores$naive_winner <- unlist(manifesto.scores$naive_winner)
+# manifesto.winner <- manifesto.scores %>% select(countryname, date, naive_winner)
+compare.leader <- merge(parl.winner, manifesto.scores) %>%
+  select(countryname, data, date, naive_winner, party_name_english)
 
 format_partyname <- function(text) {
   text = as.character(text)
-  text = str_replace(text, "Austrian People's Party", "Österreichische Volkspartei")
+  text = str_replace(text, "New Zealand Labour Party", "Labour Party")
+  text = str_replace(text, "New Zealand National Party", "National Party")
   text = str_to_lower(text)
   text = str_replace(text, "party", "")
   text = str_replace(text, "['’’']", "")
+  
   text = trimws(text)
   return(text)
 }
 
-compare.leader %>% 
-  filter(format_partyname(party_name_english) != format_partyname(party_name)) %>%
+diff <- compare.leader %>% 
+  filter(format_partyname(party_name_english) != format_partyname(naive_winner)) %>%
   filter(countryname != "Ireland") %>%
-  filter(countryname != "Italy") %>% View()
+  filter(countryname != "Italy")
+  # filter(countryname == "Bulgaria")
+View(diff)
 
 
 
@@ -128,20 +244,25 @@ dpi$countryname[dpi$countryname == "S. Africa"] <- "South Africa"
 dpi$countryname[dpi$countryname == "S. Korea"] <- "South Korea"
 
 ## Compare Data With Manifesto Project -------------------------------------------
-
 dpi_manifesto <- merge(dpi, manifesto.scores)
-model <- polr(dpi.polarization~polarization, data = dpi_manifesto)
-
+model <- polr(dpi.polarization~naive_polarization, data = dpi_manifesto)
+summary(model)
 
 # Visualizations -----------------------------------------------------------------
 manifesto.scores %>% group_by(countryname) %>% filter(n() > 10) %>%
   ggplot(aes(date, polarization)) + geom_line() + facet_wrap(~countryname)
 
 
+manifesto.scores %>% filter(countryname == "Netherlands") %>%
+  ggplot(aes(date)) + 
+  geom_line(aes(y = polarization), color = "blue") + 
+  geom_line(aes(y = naive_polarization), color = "red", alpha = .9)
+
 # View change of party overtime
 manifesto %>% group_by(party, partyname) %>% summarise(diff = range(rile)) %>% arrange(desc(diff))
-manifesto %>% filter(party == 34210) %>%
-  ggplot(aes(date, rile)) + geom_line()
+manifesto %>% filter(party == 61320) %>%
+  ggplot(aes(date, rile)) + geom_line() +
+  geom_hline(yintercept = 0, color="red")
 
 manifesto.scores %>% arrange(polarization)
 
